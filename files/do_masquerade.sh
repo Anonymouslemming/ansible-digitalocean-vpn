1705a6ce4bad3d1e56b32f431900c8baf766a826
Wayne <wayne@penguinpowered.org>
2016-11-25 16:20:03 +0000

    Added full path to iptables and added exit 0 to ensure successful run by OpenVPN

--- files/do_masquerade.sh
+++ files/do_masquerade.sh
@@ -4,6 +4,7 @@
 # Manual changes will be lost.
 
 # Don't add masquerading rule if it is already exists
-iptables -n -t nat -L POSTROUTING | grep -q MASQUERADE && exit 0 ||:
+/sbin/iptables -n -t nat -L POSTROUTING | /bin/grep -q MASQUERADE && exit 0 ||:
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
+exit 0
